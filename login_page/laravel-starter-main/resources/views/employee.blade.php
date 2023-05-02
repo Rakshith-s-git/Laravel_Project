819519a682e9604096e62e585448c9ce725f850c
Rakshith-s-git <raskhiths166@gmail.com>
2023-05-02 16:43:09 +0530

    Laravel_Project

---
+++ login_page/laravel-starter-main/resources/views/employee.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+   <div>
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+            <div class="container mx-auto px-6 py-8">
+            <h3 class="text-gray-700 text-3xl font-medium">Welcome : <strong class="font-black">Employee Dashboard </strong></h3>
+
+                <iframe width=100% height=900px frameBorder="0" src="http://192.168.29.230:8088/superset/dashboard/12/?standalone=true"> </iframe>
+
+            </div>
+        </main>
+    </div>
+</div>
+</x-app-layout>
